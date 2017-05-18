5c7e9ad09f0dda6f0e36493f5589d5e033a82b58
liuchangling5 <liuchangling1995@163.com>
2017-05-18 15:13:14 +0800

    搜索页改动

--- app/src/main/java/com/example/administrator/metrobao/surround/FirstFragment.java
+++ app/src/main/java/com/example/administrator/metrobao/surround/FirstFragment.java
@@ -1,13 +1,13 @@
 package com.example.administrator.metrobao.surround;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
-import com.example.administrator.metrobao.R;
+        import com.example.administrator.metrobao.R;
 
 /**
  * Created by Administrator on 2017/5/17 0017.
