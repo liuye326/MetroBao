bcf4daff7cce8168361977905e6062dc0d6aa7a0
liuchangling5 <liuchangling1995@163.com>
2017-05-25 16:19:48 +0800

    实现页面之间的跳转返回，列表页修改等

---
+++ app/src/main/java/com/example/administrator/metrobao/surround/Search/Mainpage.java
@@ -0,0 +1,16 @@
+package com.example.administrator.metrobao.surround.Search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.administrator.metrobao.R;
+
+/**
+ * Created by Administrator on 2017/5/25.
+ */
+public class Mainpage extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage_layout);
+    }
+}
