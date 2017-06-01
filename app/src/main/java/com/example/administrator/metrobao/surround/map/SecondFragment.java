90063db6a4d7601b498e94b7d8e53f666fb4341e
刘晔 <liuye326@sina.com>
2017-06-01 09:09:40 +0800

    部分代码上传

--- app/src/main/java/com/example/administrator/metrobao/surround/map/SecondFragment.java
+++ app/src/main/java/com/example/administrator/metrobao/surround/map/SecondFragment.java
@@ -11,8 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
@@ -72,4 +74,18 @@ public class SecondFragment extends Fragment implements LocationSource,AMapLocat
         // aMap.setMyLocationType()
     }
 
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+
+    }
+
+    @Override
+    public void deactivate() {
+
+    }
 }
