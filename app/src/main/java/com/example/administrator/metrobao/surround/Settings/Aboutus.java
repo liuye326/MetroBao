dd53539d326677af1d738fab1164f306eb0097b3
liuchangling5 <liuchangling1995@163.com>
2017-06-08 09:25:25 +0800

    完善第三个界面功能

--- app/src/main/java/com/example/administrator/metrobao/surround/Settings/Aboutus.java
+++ app/src/main/java/com/example/administrator/metrobao/surround/Settings/Aboutus.java
@@ -1,5 +1,6 @@
 package com.example.administrator.metrobao.surround.Settings;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,11 +8,12 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.example.administrator.metrobao.R;
+import com.example.administrator.metrobao.surround.MainActivity;
 
 /**
  * Created by Administrator on 2017/6/7.
  */
-public class About_us extends AppCompatActivity {
+public class Aboutus extends Activity{
 
     private ImageView back;
 
@@ -31,7 +33,8 @@ public class About_us extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent();
-                i.putExtra("radio_masg",2);
+                i.setClass(Aboutus.this, MainActivity.class);
+                i.putExtra("radio_setting",2);
                 startActivity(i);
             }
         });
